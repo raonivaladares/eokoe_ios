98f9f6f2f898725586d6e1559d49c2799decfb1c
rov <valadares.thel@gmail.com>
2017-04-13 11:47:53 -0300

    first commit

---
+++ eokoe_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eokoe_ios
+//
+//  Created by Raoni Valadares on 4/13/17.
+//  Copyright © 2017 rov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
