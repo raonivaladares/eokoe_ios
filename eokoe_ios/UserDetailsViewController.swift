0db659fc1206eb970b85e7ab00c9f0c56ca6e169
rov <valadares.thel@gmail.com>
2017-04-14 21:36:50 -0300

    Adds UserDetails outlets

--- eokoe_ios/UserDetailsViewController.swift
+++ eokoe_ios/UserDetailsViewController.swift
@@ -2,6 +2,13 @@ import UIKit
 
 class UserDetailsViewController: UIViewController {
   
+  @IBOutlet weak var coverImageView: UIImageView!
+  @IBOutlet weak var profileImageView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var emailLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+  
   override func viewDidLoad() {
     self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
     self.navigationController?.navigationBar.shadowImage = UIImage()
@@ -12,8 +19,8 @@ class UserDetailsViewController: UIViewController {
   
   override func viewWillDisappear(_ animated: Bool) {
     
-    navigationController?.navigationBar.setBackgroundImage(UINavigationBar.appearance().backgroundImage(for: UIBarMetrics.default), for:UIBarMetrics.default)
     navigationController?.navigationBar.isTranslucent = UINavigationBar.appearance().isTranslucent
+    navigationController?.navigationBar.setBackgroundImage(UINavigationBar.appearance().backgroundImage(for: UIBarMetrics.default), for:UIBarMetrics.default)
     navigationController?.navigationBar.shadowImage = UINavigationBar.appearance().shadowImage
   }
   
